fb82548902aca12804928d6d2484847e57361491
Galina <gana88tit@gmail.com>
2023-10-11 11:19:19 +0300

    changes in the project

---
+++ src/main/java/Epic.java
@@ -0,0 +1,21 @@
+public class Epic extends Task {
+    protected String[] subtasks;
+
+    public Epic(int id, String[] subtasks) {
+        super(id);
+        this.subtasks = subtasks;
+    }
+
+    public String[] getSubtasks() {
+        return subtasks;
+    }
+
+    public boolean matches(String query)
+}
+
+
+
+
+
+
+
